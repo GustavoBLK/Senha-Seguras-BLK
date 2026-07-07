c29d6f1f09a3bf6b96818af220e1b6dfed9c40cf
GustavoBLK <gustavo.pereira.souza04@escola.pr.gov.br>
2026-07-07 17:16:01 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -9,12 +9,30 @@ botoes[1].onclick = aumenTamanho;8
 
 function diminuiTamanho(){
     if (tamanhoSenha > 1){
-        tamanhoSenha--;
+        tamanhoSenha--; 
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 function aumentaTamanho(){
     if (tamanhoSenha < 20)
     tamanhoSenha++;
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+
+
+const campoSenha = document.querySelector('#campoSenha');
+
+const letrasMaiuculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+geraSenha();
+
+function geraSenha(){
+    let senha = '';
+    for (let i=0; i <tamanhoSenha;i++) {
+    let numeroAleatorio = Math.random()*letrasMaiusculas.length
+    numeroAleatorio = Math.floor(numeroAleatorio);
+    senha = senha + letrasMaiuculas[numeroAleatorio];
+    }
+    campoSenha.value = senhas;
 }
